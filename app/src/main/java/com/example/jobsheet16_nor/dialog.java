df075a0a1bf95779141d5b403d6b34e6b25fb8bf
NorHidayah <salma@gmail.com>
2022-10-24 10:03:09 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet16_nor/dialog.java
@@ -0,0 +1,14 @@
+package com.example.jobsheet16_nor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+
+    }
+}
